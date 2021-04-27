e6b29d27ed5ce5e0c8ec7615869224fa4988ef79
Dibyakanti <dibyakantikumar1999@gmail.com>
2021-04-27 17:14:02 +0530

    Added keys

--- keys.py
+++ keys.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-consumer_key = 'NrF5bt4MhnGzdKyehnwiwtXbo'
-consumer_secret = 'ne3HRfxehhmtlQsVLKxz4zupOXUu9VHUMYbKV07tMeJsdiR39w'
+consumer_key = 'put consumer_key'
+consumer_secret = 'put consumer_secret'
 
-access_token = '1289826632903979008-R3qr2tVshwnAiHONMmcwJuRpGPpVGd'
-access_token_secret = 'NM021bBrjwVBlag3UO8KLx7biTDwjTMXG5F1NYMz9E8DJ'
+access_token = 'put access_token'
+access_token_secret = 'put access_token_secret'
